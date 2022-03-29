940a1fcc5916724eeee9f89f19b914da27115785
mark199850 <mark199850@gmail.com>
2022-03-29 10:56:15 +0200

    uploaded

---
+++ StudentProject/StudentProject/Controllers/StudentController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentProject.Models;
+using StudentProject.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentProject.Controllers
+{
+    [Controller]
+     [Route("[controller]")]
+    public class StudentController : Controller
+    {
+        private readonly StudentService studentService;
+        
+        public StudentController(StudentService studentService)
+        { 
+            this.studentService = studentService;
+        }
+
+        [HttpGet]
+        public IEnumerable<Student> GetAllStudent()
+        {
+            return studentService.GetAllStudent();
+        }
+
+        [HttpGet]
+        [Route("'{id}")]
+        public Student GetAllStudent(int id)
+        {
+            return studentService.GetStudentByEmail(id);
+        }
+    }
+}
---
+++ StudentProject/StudentProject/Services/StudentService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentProject.Services
+{
+    public class StudentService
+    {
+        private readonly school2Context context;
+        
+        public StudentService(school2Context context)
+        {
+            this.context = context;
+            context.ChangeTracker.LazyLoadingEnabled = false;
+        }
+
+        public IEnumerable<Student> GetAllStudent()
+        {
+            return context.Students.Include(student => student.Subjects).ToList();
+
+        }
+
+        public Student GetStudentByEmail(int id)
+        {
+            return context.Students.Include(s => s.Subjects).First(s => s.Id == id);
+        }
+    }
+}
